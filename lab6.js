4df64406aacc69a4b36ca5c93ba9de3736809fdd
Студент Факультета Бизнеса <miha040222@gmail.com>
2022-11-29 11:06:16 +0700

    Alert

--- lab6.js
+++ lab6.js
@@ -13,13 +13,9 @@ function setRussian() {
     document.getElementById('your-age').innerHTML = 'Ваша возраст'; 
     document.getElementById('input-age').placeholder = 'введите возраст';
 
-    document.getElementById('ok-button').innerHTML = 'Your age'; 
-    document.getElementById('input-age').placeholder = 'enter your age';
-
     document.getElementById('ok-button').value = 'ок'; 
 
     document.getElementById('reset-button').value = 'сброс'; 
-    
 
 }
 
@@ -42,4 +38,16 @@ function setEnglish() {
 
     document.getElementById('reset-button').value = 'reset'; 
     
+}
+
+function showAlert() {
+    let userName = document.getElementById('input-name').value;
+    let userSurname = document.getElementById('input-surname').value;
+    let userAge = document.getElementById('input-age').value;
+    alert('Привет, ' + userName + ' ' + userSurname + '\n' + 'Ваш возраст: ' + userAge);
+    let r = Math.round(Math.random()*255);
+    let g = Math.round(Math.random()*255);
+    let b = Math.round(Math.random()*255);
+    let color = 'rgb(' + r + ',' + g + ',' + b + ')';
+    document.getElementById('user-data').style.backgroundColor = color;
 }
