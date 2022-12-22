3de615f8e9405844564dc822ad572aced94ae867
Студент Факультета Бизнеса <miha040222@gmail.com>
2022-12-22 17:09:32 +0700

    Количество дней с даты рождения

--- lab8.js
+++ lab8.js
@@ -4,4 +4,14 @@ function ShowDate(params) {
     out.innerHTML = today.toLocaleDateString ();
     }
 
+    function showDaysCount(parasm) {
+        let today = new Date();
+        let inputDate = document.querySelector('input[type=date]');
+        let birthday = new Date (inputDate.value);
+        let daysCount = (today - birthday)/1000/60/60/24;
+        let result = document.getElementById('result');
+        daysCount = Math.floor(daysCount); // округляем в меньшую сторону
+        result.innerHTML = 'С даты рождения прошло дней:' + daysCount;   
+    }
+
 window.addEventListener('load', ShowDate);
