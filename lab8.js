372c97e0e72748c61caa34982359ec46689aa479
Zhukov Oleg <miha040222@gmail.com>
2022-12-27 23:51:22 +0700

    Круглая Дата и Улучшенные дата и время

--- lab8.js
+++ lab8.js
@@ -1,35 +1,39 @@
-function ShowDate(params) {
-    let out = document.getElementById('today'); 
-    let today = new Date ();
-    out.innerHTML = today.toLocaleDateString ();
-    }
+function showDate(params) {
+    let out = document.getElementById('today');
+    let today = new Date();
+    out.innerHTML = today.toLocaleDateString();
+}
 
-    function showDaysCount(parasm) {
-        let today = new Date();
-        let inputDate = document.querySelector('input[type=date]');
-        let birthday = new Date (inputDate.value);
-        let daysCount = (today - birthday)/1000/60/60/24;
-        let result = document.getElementById('result');
-        daysCount = Math.floor(daysCount); 
-        result.innerHTML = 'С даты рождения прошло дней:' + daysCount;   
-    }
+function showDaysCount(params) {
+    let today = new Date();
+    let inputDate = document.querySelector('input[type=date]');
+    let birthday = new Date(inputDate.value);
+    let daysCount = (today - birthday)/1000/60/60/24;
+    let result = document.getElementById('result');
+    daysCount = Math.ceil(daysCount);
+    result.innerHTML = 'С даты рождения прошло дней: ' + daysCount;
+}
 
-    function showTime() {
-        let outTime = document.getElementById('time');
-        let currentTime = new Date();
-        outTime.innerHTML = currentTime.toLocaleTimeString('ru');
-    }
-    window.addEventListener('load', showTime);
-    setInterval(showTime, 1000); 
+window.addEventListener('load', showDate);
+
+function showTime() {
+    let outTime = document.getElementById('time');
+    let currentTime = new Date();
+    outTime.innerHTML = currentTime.toLocaleTimeString('ru');
+}
+
+window.addEventListener('load', showTime);
+setInterval(showTime, 1000);
+
+function showNewDate(params) {
+    let inputDate = document.querySelector('input[type=date]');
+    let birthday = new Date(inputDate.value);
     
-    function showNewDate(params) {
-        let inputDate = document.querySelector('input[type=date]');
-        let birthday = new Date(inputDate.value);
     
-        let days = document.querySelector('input[type=number]').value;
-    
-        let NewDate = birthday.getTime() + days*24*60*60*1000;
-        NewDate = new Date(NewDate);
-    
-        resultdays.innerHTML = 'Круглая дата:  ' + NewDate.toLocaleDateString("ru-RU");
-    }
+    let days = document.querySelector('input[type=number]').value;
+
+    let NewDate = birthday.getTime() + days*24*60*60*1000;
+    NewDate = new Date(NewDate);
+   
+    resultdays.innerHTML = 'Круглая дата:  ' + NewDate;
+}
