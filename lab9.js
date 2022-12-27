50ff9b7186e22beb85764d9f941519f8ad3d3a96
Zhukov Oleg <miha040222@gmail.com>
2022-12-28 00:09:44 +0700

    Кнопка Имя-Фамилия

---
+++ lab9.js
@@ -0,0 +1,14 @@
+function clickMe(button) {
+    if(button.innerHTML == 'Щелчок')
+        button.innerHTML = 'Спасибо';
+    else
+    button.innerHTML = 'Щелчок'
+}
+function student(button) {
+    if(button.innerHTML == 'Жуков')
+        button.innerHTML = 'Олег';
+    else
+    button.innerHTML = 'Жуков'
+}
+
+
