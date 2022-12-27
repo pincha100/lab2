e3bdc4ec22f4f6973b2ef1b4fb0ddbd2bc106eca
Zhukov Oleg <miha040222@gmail.com>
2022-12-28 00:34:21 +0700

    Переключения состояний

--- lab9.js
+++ lab9.js
@@ -11,4 +11,25 @@ function student(button) {
     button.innerHTML = 'Жуков'
 }
 
+function knopka(button) {
+    if(button.innerHTML == 'Нажми меня') {
+        button.innerHTML = 'Ещё раз нажми';
+        button.style.color = 'blue';
+    }
+        
+    else if(button.innerHTML == 'Ещё раз нажми') {
+        button.innerHTML = 'Ещё';  
+        button.style.color = 'green';
+    }
+         
+    else if(button.innerHTML == 'Ещё') {
+        button.innerHTML = 'Начать сначала';   
+        button.style.color = 'red';
+    }
+        
+    else {
+        button.innerHTML = 'Нажми меня' 
+        button.style.color = 'yellow'; 
+    }
+}
 
